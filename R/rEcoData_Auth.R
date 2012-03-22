316f0867bef8480e79f408e0909e34c5005bd080
Karthik Ram <karthik.ram@gmail.com>
2012-03-22 00:14:23 -0700

    added documentation skeleton.

--- R/rEcoData_Auth.R
+++ R/rEcoData_Auth.R
@@ -1,2 +1,14 @@
-rEcoData_Auth <-
-function(key=getOption("EcoData")) { }
+#'<brief desc>
+#'
+#'<longer description>
+#'@param key Does EcoData even need an API Key? Guess not?
+#'@keywords
+#'@seealso
+#'@return
+#'@alias
+#'@export
+#'@examples \dontrun{
+#'
+#'}
+rEcoData_Auth <- function(key=getOption("EcoData")) {
+}
